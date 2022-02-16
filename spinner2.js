b134600598f1d936af50a22bfc424beea1e700c8
ConsensusAI <malik.iqbal@hotmail.ca>
2022-02-16 23:46:12 +0000

    Add new line after spinner

--- spinner2.js
+++ spinner2.js
@@ -4,9 +4,12 @@ let revolutions = 0;
 while (revolutions < 2) {
   for (const char of spin) {
     delay += 200;
-      setTimeout(() => {
-        process.stdout.write('\r' + char + '   ')
-      }, delay);
+    setTimeout(() => {
+      process.stdout.write("\r" + char + "   ");
+    }, delay);
   }
   revolutions++;
 }
+setTimeout(() => {
+  process.stdout.write("\n");
+}, delay + 50);
